4c0ef6c1d9e1327092bf1f2df128791a0065fd94
thejoylee <62118591+thejoylee@users.noreply.github.com>
2020-03-12 20:15:41 -0400

    Update map.js

--- map.js
+++ map.js
@@ -5,3 +5,9 @@ console.log('Loaded map.js')
 
 // your mapbox token
 mapboxgl.accessToken = 'pk.eyJ1IjoidGhlam95bGVlIiwiYSI6ImNrN3BmZnZ5NDBqYjQzbW1pNGs0MWFiejkifQ.9stnbzBJqom6XYjX73VYcA'
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: [-73.96024, 40.80877],
+    zoom: 1
+})
