76018ee68d2919d40e15732c5c73f255f4d2261a
thejoylee <62118591+thejoylee@users.noreply.github.com>
2020-03-12 20:15:07 -0400

    Update map.js

--- map.js
+++ map.js
@@ -1,1 +1,7 @@
-console.log("Hello, world!")
+'use strict'        // let the browser know we're serious
+
+// debug statement letting us know the file is loaded
+console.log('Loaded map.js')
+
+// your mapbox token
+mapboxgl.accessToken = 'pk.eyJ1IjoidGhlam95bGVlIiwiYSI6ImNrN3BmZnZ5NDBqYjQzbW1pNGs0MWFiejkifQ.9stnbzBJqom6XYjX73VYcA'
